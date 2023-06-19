5c951831b9c1f1bfb06c415de68f347950797845
Benson Kiai <benson.kiai86@gmail.com>
2023-06-20 01:01:17 +0300

    Add function to closemenu on each links click

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const menuBtn = document.getElementById('headermenubtn');
 
 const closeBtn = document.getElementById('close-btn');
 
+const navLink = document.querySelectorAll('.nav-link');
+
 menuBtn.addEventListener('click', () => {
   navMenu.style.display = 'block';
 });
 
 closeBtn.addEventListener('click', () => {
   navMenu.style.display = 'none';
+});
+
+navLink.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    navMenu.style.display = "none";
+  });
 });
