19af35688c45dc812baf5aecbd0d8146438d066c
Benson Kiai <benson.kiai86@gmail.com>
2023-06-19 21:16:32 +0300

    Add JS for drop down mobile menu

---
+++ script.js
@@ -0,0 +1,13 @@
+const navMenu = document.querySelector(".nav-menu");
+
+const menuBtn = document.getElementById("headermenubtn");
+
+// const closeBtn = document.getElementById("close-btn");
+
+menuBtn.addEventListener("click", () => {
+    navMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    navMenu.style.display = "none";
+});
