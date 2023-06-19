d2887cdce3c85c2e8dda79c8a053af0066937669
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-06-19 16:33:00 -0300

    update closeBtn variable

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navMenu = document.querySelector(".nav-menu");
 
 const menuBtn = document.getElementById("headermenubtn");
 
-// const closeBtn = document.getElementById("close-btn");
+const closeBtn = document.getElementById("close-btn");
 
 menuBtn.addEventListener("click", () => {
     navMenu.style.display = "block";
