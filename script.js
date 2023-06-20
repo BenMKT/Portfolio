a318e4df022b958e16cad8931acdd502270354d2
Benson Kiai <benson.kiai86@gmail.com>
2023-06-20 22:14:28 +0300

    Add popups and buttons dynamically

--- script.js
+++ script.js
@@ -131,3 +131,129 @@ worksection.forEach((project) => {
       </div>`;
 });
 
+const popupContainer = document.querySelector(".popupContainer");
+
+function createPop() {
+  for (let i = 0; i < 4; i += 1) {
+    const pop = document.createElement("pop");
+    pop.classList.add(`popup${i}`);
+    pop.innerHTML = `
+ <div class='grid-item1 tonic' id='grid-item1 tonic'>
+    <nav>
+      <h2>${worksection[i].Tonic}</h2>
+      <button type='button' class='closepopup'><img src="images/Close-Icon1.png" alt="close button"></button>
+    </nav>
+    <div>
+    <ul class="flexy">
+      <li class="canopy">${worksection[i].offer[0]}</li>
+      <li class="backend">${worksection[i].offer[1]}</li>
+      <li class="year">${worksection[i].offer[2]}</li>
+    
+    </ul>
+    </div>
+    <div class="image1">
+      <img src="${worksection[i].snapshoot}" alt="project snapshot1">
+    </div>
+    
+    <div class="body1">
+      <p class="tonic">${worksection[i].descriptionpop1}</p>
+      </div>
+      <div class="poplistbtn">
+      <div>
+        <ul class="linkss">
+          <li>${worksection[i].skills[0]}</li>
+          <li>${worksection[i].skills[1]}</li>
+          <li>${worksection[i].skills[2]}</li>
+        </ul>
+      </div>
+        <div class="seelive">
+          <button>See live <img class="imglive" src="${worksection[i].liveversion}" alt="live"></button>
+          <button>See Source <img class="imglive" src="${worksection[i].source}" alt="source"></button>
+        </div>
+        </div>
+      </div>
+      `;
+    popupContainer.appendChild(pop);
+  }
+}
+createPop();
+
+const seabtn = document.querySelectorAll("#prjbtn");
+const cancelbtn = document.querySelectorAll(".closepopup");
+const overlay = document.getElementById("overlay");
+
+seabtn[0].addEventListener("click", () => {
+  const popup0 = document.querySelector(".popup0");
+  popup0.classList.add("active");
+  overlay.classList.add("active");
+});
+
+function close(popup0) {
+  if (popup0 == null) return;
+  popup0.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+cancelbtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const popup0 = button.closest(".popup0");
+    close(popup0);
+  });
+});
+
+seabtn[1].addEventListener("click", () => {
+  const popup1 = document.querySelector(".popup1");
+  popup1.classList.add("active");
+  overlay.classList.add("active");
+});
+
+function close1(popup1) {
+  if (popup1 == null) return;
+  popup1.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+cancelbtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const popup1 = button.closest(".popup1");
+    close1(popup1);
+  });
+});
+
+seabtn[2].addEventListener("click", () => {
+  const popup2 = document.querySelector(".popup2");
+  popup2.classList.add("active");
+  overlay.classList.add("active");
+});
+
+function close2(popup2) {
+  if (popup2 == null) return;
+  popup2.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+cancelbtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const popup2 = button.closest(".popup2");
+    close2(popup2);
+  });
+});
+
+seabtn[3].addEventListener("click", () => {
+  const popup3 = document.querySelector(".popup3");
+  popup3.classList.add("active");
+  overlay.classList.add("active");
+});
+
+function close3(popup3) {
+  if (popup3 == null) return;
+  popup3.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+cancelbtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const popup3 = button.closest(".popup3");
+    close3(popup3);
+  });
+});
