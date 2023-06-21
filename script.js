32e82b87f5898b2c34f22897871361913aac9c3b
Benson Kiai <benson.kiai86@gmail.com>
2023-06-21 18:06:34 +0300

    Add form  validation script at the bottom

--- script.js
+++ script.js
@@ -257,3 +257,54 @@ cancelbtn.forEach((button) => {
     close3(popup3);
   });
 });
+
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", (event) => {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.innerHTML = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  // if the email field is valid, we let the form submit
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.innerHTML = "You need to enter a valid email address.";
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.innerHTML = "Entered value needs to be an email address.";
+  } else if (email.validity.patternMismatch) {
+    // If the data doesn't match the assigned pattern,
+    // display the following error message.
+    emailError.innerHTML = 'Email should be in lowercase.';
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+  console.log(emailError.classList);
+}
