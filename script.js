52c6b7e0ede2528e1cb5f983e0425744e10e0e85
Benson Kiai <benson.kiai86@gmail.com>
2025-08-20 10:54:54 +0300

    Refactor ESLint configuration by removing Babel parser settings and update package.json to include lint script for improved code quality checks

--- script.js
+++ script.js
@@ -1,93 +1,95 @@
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector('.nav-menu');
 
-const menuBtn = document.getElementById("headermenubtn");
+const menuBtn = document.getElementById('headermenubtn');
 
-const closeBtn = document.getElementById("close-btn");
+const closeBtn = document.getElementById('close-btn');
 
-const navLink = document.querySelectorAll(".nav-link");
+const navLink = document.querySelectorAll('.nav-link');
 
-menuBtn.addEventListener("click", () => {
-  navMenu.style.display = "block";
+menuBtn.addEventListener('click', () => {
+  navMenu.style.display = 'block';
 });
 
-closeBtn.addEventListener("click", () => {
-  navMenu.style.display = "none";
+closeBtn.addEventListener('click', () => {
+  navMenu.style.display = 'none';
 });
 
-navLink.forEach((navLink) => {
-  navLink.addEventListener("click", () => {
-    navMenu.style.display = "none";
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.style.display = 'none';
   });
 });
 // project section
 const worksection = [
   {
-    class: "grid-item1 stories single",
-    snapshoot: "images/ATP.png",
-    Tonic: "ATP App",
-    offer: ["GOVERNANCE", "Full Stack Dev", "2024"],
+    class: 'grid-item1 stories single',
+    snapshoot: 'images/ATP.png',
+    Tonic: 'ATP App',
+    offer: ['GOVERNANCE', 'Full Stack Dev', '2024'],
     description:
-      "A hybrid civic‑tech platform enabling real‑time citizen participation in governance, driving an estimated 87% increase in community engagement during pilot programs.",
-    descriptionpop1: `A full-stack hybrid platform engineered with Next.js, TypeScript, Prisma, PostgreSQL, and deployed seamlessly on Vercel. It features secure authentication via NextAuth, real-time messaging through Twilio, Redis-powered caching, and extended backend capabilities using Supabase. The frontend delivers a beautiful, animated, WCAG-compliant, responsive UI crafted with Tailwind CSS, DaisyUI, ShadcnUI, Framer Motion, and Recharts—enabling dynamic dashboards and interactive user experiences. Core features include role-based access control, encrypted data handling with Bcrypt, and scheduled tasks via Cron. Robust unit and integration testing are implemented using Jest to ensure reliability across both frontend and backend workflows.`,
-    liveversion: "images/Icon.png",
-    liveLink: "https://atparty.ke",
-    source: "images/Vector.png",
-    sourceLink: "https://github.com/BenMKT/atp-party",
-    skills: ["Jest", "Next.js", "Tailwind", "NextAuth", "TypeScript"],
-  },
-  {
-    class: "grid-item1 grid-item3 tonic description",
-    snapshoot: "images/BudgetApp Screenshot1.png",
-    Tonic: "Smart Budget App",
-    offer: ["FINANCE", "Full Stack Dev", "2023"],
-    description:
-      "A personal finance platform that delivers 20%+ gains in budgeting accuracy through automated transaction categorization and 95% test-backed coverage to ensure reliability, maintainability, and confidence in user-facing workflows.",
-    descriptionpop1: `A budget management platform developed with Ruby on Rails, PostgreSQL, and  deployed on Render. It features automated transaction categorization and robust role-based access control via Devise and CanCanCan. The codebase is backed by RSpec and Capybara tests, achieving 95% coverage to ensure reliability, maintainability, and confidence in user-facing workflows.`,
-    liveversion: "images/Icon.png",
-    liveLink: "https://smartbudgetapp-v1wv.onrender.com/",
-    source: "images/Vector.png",
-    sourceLink: "https://github.com/BenMKT/Budget-App",
-    skills: ["Rspec", "Devise", "PostgreSQL", "CanCanCan", "Ruby on Rails"],
-  },
-  {
-    class: "grid-item1 stories",
-    snapshoot: "images/SchoolFees.png",
-    Tonic: "School Fees Management System",
-    offer: ["EDUCATION", "Full Stack Dev", "2025"],
-    description:
-      "A comprehensive education‑finance platform that streamlines school fees tracking for school administrators and parents, boosting collection and reconciliation efficiency by 55% and enhancing collaborative academic performance monitoring by 74% to deliver actionable financial and educational insights.",
+      'A hybrid civic‑tech platform enabling real‑time citizen participation in governance, driving an estimated 87% increase in community engagement during pilot programs.',
     descriptionpop1:
-      "A full-stack education finance platform built with Next.js, TypeScript, Radix UI, and deployed on Vercel. It leverages PostgreSQL for structured data storage and Prisma ORM for efficient querying. Authentication is handled securely via NextAuth, with Twilio integrated for messaging and the Google Sheets API used for real-time data synchronization with administrative workflows. The frontend features a responsive interface styled with Tailwind CSS and animated using Framer Motion. Interactive dashboards powered by Recharts provide live fee tracking and academic performance insights. Additional capabilities include role-based access controls, encrypted credential handling with Bcrypt, and intuitive management tools tailored for school administrators.",
-    liveversion: "images/Icon.png",
-    liveLink: "https://school-fees-virid.vercel.app/",
-    source: "images/Vector.png",
-    sourceLink: "https://github.com/BenMKT/school-fees",
+      'A full-stack hybrid platform engineered with Next.js, TypeScript, Prisma, PostgreSQL, and deployed seamlessly on Vercel. It features secure authentication via NextAuth, real-time messaging through Twilio, Redis-powered caching, and extended backend capabilities using Supabase. The frontend delivers a beautiful, animated, WCAG-compliant, responsive UI crafted with Tailwind CSS, DaisyUI, ShadcnUI, Framer Motion, and Recharts—enabling dynamic dashboards and interactive user experiences. Core features include role-based access control, encrypted data handling with Bcrypt, and scheduled tasks via Cron. Robust unit and integration testing are implemented using Jest to ensure reliability across both frontend and backend workflows.',
+    liveversion: 'images/Icon.png',
+    liveLink: 'https://atparty.ke',
+    source: 'images/Vector.png',
+    sourceLink: 'https://github.com/BenMKT/atp-party',
+    skills: ['Jest', 'Next.js', 'Tailwind', 'NextAuth', 'TypeScript'],
+  },
+  {
+    class: 'grid-item1 grid-item3 tonic description',
+    snapshoot: 'images/BudgetApp Screenshot1.png',
+    Tonic: 'Smart Budget App',
+    offer: ['FINANCE', 'Full Stack Dev', '2023'],
+    description:
+      'A personal finance platform that delivers 20%+ gains in budgeting accuracy through automated transaction categorization and 95% test-backed coverage to ensure reliability, maintainability, and confidence in user-facing workflows.',
+    descriptionpop1:
+      'A budget management platform developed with Ruby on Rails, PostgreSQL, and  deployed on Render. It features automated transaction categorization and robust role-based access control via Devise and CanCanCan. The codebase is backed by RSpec and Capybara tests, achieving 95% coverage to ensure reliability, maintainability, and confidence in user-facing workflows.',
+    liveversion: 'images/Icon.png',
+    liveLink: 'https://smartbudgetapp-v1wv.onrender.com/',
+    source: 'images/Vector.png',
+    sourceLink: 'https://github.com/BenMKT/Budget-App',
+    skills: ['Rspec', 'Devise', 'PostgreSQL', 'CanCanCan', 'Ruby on Rails'],
+  },
+  {
+    class: 'grid-item1 stories',
+    snapshoot: 'images/SchoolFees.png',
+    Tonic: 'School Fees Management System',
+    offer: ['EDUCATION', 'Full Stack Dev', '2025'],
+    description:
+      'A comprehensive education‑finance platform that streamlines school fees tracking for school administrators and parents, boosting collection and reconciliation efficiency by 55% and enhancing collaborative academic performance monitoring by 74% to deliver actionable financial and educational insights.',
+    descriptionpop1:
+      'A full-stack education finance platform built with Next.js, TypeScript, Radix UI, and deployed on Vercel. It leverages PostgreSQL for structured data storage and Prisma ORM for efficient querying. Authentication is handled securely via NextAuth, with Twilio integrated for messaging and the Google Sheets API used for real-time data synchronization with administrative workflows. The frontend features a responsive interface styled with Tailwind CSS and animated using Framer Motion. Interactive dashboards powered by Recharts provide live fee tracking and academic performance insights. Additional capabilities include role-based access controls, encrypted credential handling with Bcrypt, and intuitive management tools tailored for school administrators.',
+    liveversion: 'images/Icon.png',
+    liveLink: 'https://school-fees-virid.vercel.app/',
+    source: 'images/Vector.png',
+    sourceLink: 'https://github.com/BenMKT/school-fees',
     skills: [
-      "Next.js",
-      "TypeScript",
-      "Recharts",
-      "Tailwind",
-      "Google Sheets API",
+      'Next.js',
+      'TypeScript',
+      'Recharts',
+      'Tailwind',
+      'Google Sheets API',
     ],
   },
   {
-    class: "grid-item1 tonic description",
-    snapshoot: "images/Space Travellers Hub.png",
-    Tonic: "Space Travelers Hub",
-    offer: ["TRAVEL", "Front End Dev", "2023"],
+    class: 'grid-item1 tonic description',
+    snapshoot: 'images/Space Travellers Hub.png',
+    Tonic: 'Space Travelers Hub',
+    offer: ['TRAVEL', 'Front End Dev', '2023'],
     description:
       "A dynamic space exploration platform that integrates SpaceX's live API to offer real-time booking for commercial and scientific space travel services.",
     descriptionpop1:
       "Developed through pair programming, Space Travelers' Hub is a single-page React application powered by Redux Toolkit and RTK Query for efficient state management and API consumption. Built with Vite for rapid development and optimized bundling, the platform interfaces with the SpaceX REST API to enable users to browse and reserve rockets and missions. Bookings are managed in a centralized Redux store, ensuring consistent state across views. The codebase is rigorously tested with Vitest to maintain reliability and confidence in user interactions.",
-    liveversion: "images/Icon.png",
-    liveLink: "https://guileless-halva-645039.netlify.app/",
-    source: "images/Vector.png",
-    sourceLink: "https://github.com/BenMKT/Space-Travelers-Hub",
-    skills: ["CSS3", "React", "Redux", "Vitest", "JavaScript"],
+    liveversion: 'images/Icon.png',
+    liveLink: 'https://guileless-halva-645039.netlify.app/',
+    source: 'images/Vector.png',
+    sourceLink: 'https://github.com/BenMKT/Space-Travelers-Hub',
+    skills: ['CSS3', 'React', 'Redux', 'Vitest', 'JavaScript'],
   },
 ];
 
-const workscard = document.getElementById("works");
+const workscard = document.getElementById('works');
 
 worksection.forEach((project) => {
   workscard.innerHTML += `
@@ -117,11 +119,11 @@ worksection.forEach((project) => {
       </div>`;
 });
 // popup section
-const popupContainer = document.querySelector(".popupContainer");
+const popupContainer = document.querySelector('.popupContainer');
 
 function createPop() {
   for (let i = 0; i < 4; i += 1) {
-    const pop = document.createElement("pop");
+    const pop = document.createElement('pop');
     pop.classList.add(`popup${i}`);
     pop.innerHTML = `
     <div class='grid-item1 tonic' id='grid-item1 tonic'>
@@ -166,116 +168,116 @@ function createPop() {
 }
 createPop();
 
-const seabtn = document.querySelectorAll("#prjbtn");
-const sourcebtn = document.querySelectorAll("#sourceLink");
-const livebtn = document.querySelectorAll("#liveLink");
-const cancelbtn = document.querySelectorAll(".closepopup");
-const overlay = document.getElementById("overlay");
+const seabtn = document.querySelectorAll('#prjbtn');
+const sourcebtn = document.querySelectorAll('#sourceLink');
+const livebtn = document.querySelectorAll('#liveLink');
+const cancelbtn = document.querySelectorAll('.closepopup');
+const overlay = document.getElementById('overlay');
 
-seabtn[0].addEventListener("click", () => {
-  const popup0 = document.querySelector(".popup0");
-  popup0.classList.add("active");
-  overlay.classList.add("active");
+seabtn[0].addEventListener('click', () => {
+  const popup0 = document.querySelector('.popup0');
+  popup0.classList.add('active');
+  overlay.classList.add('active');
 });
 
 function close(popup0) {
   if (popup0 == null) return;
-  popup0.classList.remove("active");
-  overlay.classList.remove("active");
+  popup0.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 cancelbtn.forEach((button) => {
-  button.addEventListener("click", () => {
-    const popup0 = button.closest(".popup0");
+  button.addEventListener('click', () => {
+    const popup0 = button.closest('.popup0');
     close(popup0);
   });
 });
 
-sourcebtn[0].addEventListener("click", () => {
-  window.open(`${worksection[0].sourceLink}`, "_blank");
+sourcebtn[0].addEventListener('click', () => {
+  window.open(`${worksection[0].sourceLink}`, '_blank');
 });
 
-livebtn[0].addEventListener("click", () => {
-  window.open(`${worksection[0].liveLink}`, "_blank");
+livebtn[0].addEventListener('click', () => {
+  window.open(`${worksection[0].liveLink}`, '_blank');
 });
 
-seabtn[1].addEventListener("click", () => {
-  const popup1 = document.querySelector(".popup1");
-  popup1.classList.add("active");
-  overlay.classList.add("active");
+seabtn[1].addEventListener('click', () => {
+  const popup1 = document.querySelector('.popup1');
+  popup1.classList.add('active');
+  overlay.classList.add('active');
 });
 
 function close1(popup1) {
   if (popup1 == null) return;
-  popup1.classList.remove("active");
-  overlay.classList.remove("active");
+  popup1.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 cancelbtn.forEach((button) => {
-  button.addEventListener("click", () => {
-    const popup1 = button.closest(".popup1");
+  button.addEventListener('click', () => {
+    const popup1 = button.closest('.popup1');
     close1(popup1);
   });
 });
 
-sourcebtn[1].addEventListener("click", () => {
-  window.open(`${worksection[1].sourceLink}`, "_blank");
+sourcebtn[1].addEventListener('click', () => {
+  window.open(`${worksection[1].sourceLink}`, '_blank');
 });
 
-livebtn[1].addEventListener("click", () => {
-  window.open(`${worksection[1].liveLink}`, "_blank");
+livebtn[1].addEventListener('click', () => {
+  window.open(`${worksection[1].liveLink}`, '_blank');
 });
 
-seabtn[2].addEventListener("click", () => {
-  const popup2 = document.querySelector(".popup2");
-  popup2.classList.add("active");
-  overlay.classList.add("active");
+seabtn[2].addEventListener('click', () => {
+  const popup2 = document.querySelector('.popup2');
+  popup2.classList.add('active');
+  overlay.classList.add('active');
 });
 
 function close2(popup2) {
   if (popup2 == null) return;
-  popup2.classList.remove("active");
-  overlay.classList.remove("active");
+  popup2.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 cancelbtn.forEach((button) => {
-  button.addEventListener("click", () => {
-    const popup2 = button.closest(".popup2");
+  button.addEventListener('click', () => {
+    const popup2 = button.closest('.popup2');
     close2(popup2);
   });
 });
 
-sourcebtn[2].addEventListener("click", () => {
-  window.open(`${worksection[2].sourceLink}`, "_blank");
+sourcebtn[2].addEventListener('click', () => {
+  window.open(`${worksection[2].sourceLink}`, '_blank');
 });
 
-livebtn[2].addEventListener("click", () => {
-  window.open(`${worksection[2].liveLink}`, "_blank");
+livebtn[2].addEventListener('click', () => {
+  window.open(`${worksection[2].liveLink}`, '_blank');
 });
 
-seabtn[3].addEventListener("click", () => {
-  const popup3 = document.querySelector(".popup3");
-  popup3.classList.add("active");
-  overlay.classList.add("active");
+seabtn[3].addEventListener('click', () => {
+  const popup3 = document.querySelector('.popup3');
+  popup3.classList.add('active');
+  overlay.classList.add('active');
 });
 
 function close3(popup3) {
   if (popup3 == null) return;
-  popup3.classList.remove("active");
-  overlay.classList.remove("active");
+  popup3.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 cancelbtn.forEach((button) => {
-  button.addEventListener("click", () => {
-    const popup3 = button.closest(".popup3");
+  button.addEventListener('click', () => {
+    const popup3 = button.closest('.popup3');
     close3(popup3);
   });
 });
 
-sourcebtn[3].addEventListener("click", () => {
-  window.open(`${worksection[3].sourceLink}`, "_blank");
+sourcebtn[3].addEventListener('click', () => {
+  window.open(`${worksection[3].sourceLink}`, '_blank');
 });
 
-livebtn[3].addEventListener("click", () => {
-  window.open(`${worksection[3].liveLink}`, "_blank");
+livebtn[3].addEventListener('click', () => {
+  window.open(`${worksection[3].liveLink}`, '_blank');
 });
